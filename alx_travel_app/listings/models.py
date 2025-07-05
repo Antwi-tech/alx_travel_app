18d8f7ddc0752ee163e4130517bbe949d5296ba7
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-05 21:57:11 +0000

    models needed

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,11 +1,10 @@
+"""
+listings/models.py
+------------------
+Defines database tables for the *listings* app.
+
+One Listing == one travel experience / package.
+"""
+
 from django.db import models
 
-class Listing(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    location = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
