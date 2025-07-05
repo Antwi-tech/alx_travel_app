b850e036d9776326f5db79d436fd06d605d0ad8a
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-05 13:12:20 +0000

    url [ath defined properly

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -17,4 +17,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(
+    path('api/', include('listings.urls')),  # later you can define listing routes here
+]
