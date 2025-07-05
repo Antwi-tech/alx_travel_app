44b826f075c30f4ba489365b7c9ffd0ea9880a4e
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-05 22:01:43 +0000

    libraries or packages for turnin objects intoa json format

--- alx_travel_app/listings/serialisers.py
+++ alx_travel_app/listings/serialisers.py
@@ -1,7 +1,9 @@
+"""
+listings/serializers.py
+-----------------------
+Turns Listing model instances into JSON (and back) for REST endpoints.
+"""
+
 from rest_framework import serializers
 from .models import Listing
 
-class ListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Listing
-        fields = '__all__'
